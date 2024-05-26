8ebd5882026ae67f8d177d9bd965d5f4a42fd74e
Alexey <96183855+alexeich13@users.noreply.github.com>
2024-05-26 22:46:12 +0300

    Add files via upload

---
+++ OOP/lab 3/Class.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab_3
+{
+    class Set
+    {
+        public class Production
+        {
+
+            Production ID = new Production();
+            Production name = new Production();
+
+            class Developer
+            {
+                Developer FIO = new Developer();
+                Developer ID = new Developer();
+                Developer Otdel = new Developer();
+
+            }
+        };
+        public List<int> items = new List<int>();
+        public List<double> doubles = new List<double>();
+        public List<string> words = new List<string>();
+        
+        public static bool operator >(Set set, int num)
+        {
+            bool flag = false;
+            foreach (int item in set.items)
+            {
+                if (item.Equals(num)) // > - проверка на принадлежность
+                {
+                    flag = true;
+                    return flag;
+                }
+            }
+            return flag;
+        }
+
+        public static bool operator <(Set set, int num)
+        {
+            foreach (var item1 in set.items)
+            {
+                var equals = false;
+                foreach (var item2 in set.items)
+                {
+                    if (item1.Equals(item2)) // < - проверка на подмножество
+                    {
+                        equals = true;
+                        break;
+                    }
+                }
+                if (!equals)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static Set operator +(Set set1, int number)
+        {
+            set1.Add(number);
+            return set1;
+        }
+        public static Set operator +(Set set1, Set set2)
+        {
+
+            foreach (int count in set2)
+            {
+                set1.Add(count);
+            }
+            return set1;
+        }
+
+        public static Set operator *(Set set1, Set set2)
+        {
+
+            Set result1 = new Set();
+            foreach (int count1 in set1)
+            {
+                foreach (int count2 in set2)
+                {
+                    if (count2 == count1)
+                    {
+                        result1.Add(count2);
+                    }
+                }
+            }
+            return result1;
+        }
+
+
+        public static bool operator true(Set set1)
+        {
+            int count = 0;
+            foreach (var a in set1)
+            {
+                count += 1;
+            }
+            if (count < 15) return true; else return false;
+
+        }
+        public static bool operator false(Set set1)
+        {
+            int count = 0;
+            foreach (var a in set1)
+            {
+                count += 1;
+            }
+            if (count > 15) return false; else return true;
+
+        }
+
+        public static int Counter(Set set1)
+        {
+            int count = 0;
+            foreach (var a in set1)
+            {
+                count += 1;
+            }
+            return count;
+        }
+        public void Add(int item)
+        {
+            items.Add(item);
+        }
+
+        public void Add(double item)
+        {
+            if (doubles.Contains(item))
+            { //Добавление в множество
+                return;
+            }
+            doubles.Add(item);
+        }
+        public void Add(string word)
+        {
+
+            if (words.Contains(word))
+            { //Добавление в множество
+                return;
+            }
+            words.Add(word);
+        }
+        public void comma(Set set1)
+        {
+            foreach (var a in set1)
+            {
+                string b = Convert.ToString(a);
+                b = b + ',';
+                Console.Write(b);
+            }
+
+        }
+
+
+        public void Remove(int item)
+        {
+            items.Remove(item);
+        }
+
+        public void Distincter()
+        {
+            var dist = items.Distinct();
+            foreach (var a in dist)
+            {
+                Console.Write(a + " ");
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+
+    class Counter
+    {
+        public int Collection { get; set; }
+        public static explicit operator int(Counter counter)
+        {
+            return counter.Collection;
+        }
+    }
+}
---
+++ OOP/lab 3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Set set1 = new Set();
+            Set set2 = new Set();
+            Set set3 = new Set();
+            Set intnes = new Set();
+            Set set4 = new Set();
+
+
+            set2.Add(5);
+            set2.Add(8);
+            set2.Add(9);
+            set2.Add(6);
+            set2.Add(3);
+            set2.Add(2);
+            set1.Add(1);
+            set1.Add(3);
+            set1.Add(6);
+            set1.Add(8);
+            set1.Add(10);
+            set3.Add("1 ");
+            set3.Add("2 ");
+            set3.Add("3 ");
+            set3.Add("4 ");
+            set3.Add("5 ");
+            set3.Add("6 ");
+            set4.Add(12);
+            set4.Add(11);
+            set4.Add(12);
+            set4.Add(22);
+            set4.Add(33);
+            set4.Add(33);
+            set4.Add(44);
+
+            Set set1_1 = new Set();
+            Set set1_2 = new Set();
+            Set set1_3 = new Set();
+            Console.Write("прибавление ко множеству, введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            set1_1 = set1 + num;
+
+            foreach (var item in set1_1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.Write("пересечение множеств: ");
+            set1_3 = set1 * set2;
+            foreach (var item in set1_3)
+            {
+                Console.WriteLine(item);
+            }
+            
+            set1_2 = set1 + set2;
+            Console.Write("объединение множеств: ");
+            foreach (var item in set1_2)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.Write("Преобразование: ");
+            Counter counter1 = new Counter { Collection = 40 };
+            int x = (int)counter1;
+            Console.WriteLine(x);
+
+            Console.Write("false: ");
+            if (set1) Console.WriteLine(false);
+            else Console.WriteLine(true);
+
+            Console.WriteLine("добавление запятой после каждого слова: ");
+            set3.comma(set1);
+
+            Console.WriteLine("Удаление одинаковых переменных ");
+            Console.WriteLine("До: ");
+            foreach (var a in set4)
+            {
+                Console.Write(a + " ");
+            }
+            Console.Write("После: ");
+            set4.Distincter();
+
+            StaticOperation.summ(set1);
+            int max = StaticOperation.maxmin(set1);
+            Console.WriteLine("MAX-MIN=" + max);
+            int length = StaticOperation.length(set1);
+            Console.WriteLine("Длина:" + length);
+
+            int charcount = StaticOperation.CharCount("asa", 'a');
+            Console.WriteLine("string расширение:" + charcount);
+        }
+
+    }
+
+}
